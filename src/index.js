e4096fe917186aac40b957fb5370a2ee0925854c
malco <mkaplan6@illinois.edu>
2023-04-26 14:03:16 -0500

    tester

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './styles.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/kaplan'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
