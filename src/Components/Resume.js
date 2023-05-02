bc07b3372dfdae732c873754577ad0f19c27da09
malco <mkaplan6@illinois.edu>
2023-05-01 20:24:26 -0500

    Updated resume to include elden ring project

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -4,7 +4,7 @@ import banner from '.././images/banner.png';
     return (
         <div className='Res'>
             <object data = 'https://drive.google.com/file/d/1KP24nYSE6rFhWTwRwW52c__jchzXZ3CR/view?usp=sharing' type="application/pdf" width="100%" height={100}>
-            <iframe src="https://drive.google.com/file/d/1BwTOASmlMC4s7o_2_vxXqopjOQKhLnva/preview" width="1545" height="1100" allow="autoplay" title='Resume'></iframe>
+            <iframe src="https://drive.google.com/file/d/1nYMFB5j-GKCcEHAsSkusGy0R9OJVZvxe/preview" width="1545" height="1100" allow="autoplay" title='Resume'></iframe>
             </object>
             <img style={{position:"relative", bottom: -10, width:1545, height:"100%"}}src={banner} alt=""/>
         </div>
